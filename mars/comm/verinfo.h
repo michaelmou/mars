05dae47fc5c657bf80094f601983a2b94e82cda3
michael <michael@qiaodanfengdeiMac.local>
2018-03-26 19:57:14 +0800

    在mars-open-mac下，删掉不要的其它项目，剩下log跟comm

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f685e74"
-#define MARS_PATH "master"
+#define MARS_REVISION ""
+#define MARS_PATH ""
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2017-10-12 18:37:49"
-#define MARS_TAG "mars_xlog_sdk/mars_libs"
+#define MARS_BUILD_TIME "2018-04-11 14:37:04"
+#define MARS_TAG "/Users/michael/Desktop/packages_[2018-04-11_14.36]@@michael"
 
 #endif
